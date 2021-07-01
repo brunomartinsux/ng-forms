2d94921b49691651bb77102811c75f12e5ff860f
Bruno Luis Martins <zapelim.dev>
2021-07-01 16:37:27 -0300

    searchbar GeoProvider

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
+import * as GeoSearch from 'leaflet-geosearch';
 
 @Component({
   selector: 'app-map',
@@ -26,9 +28,8 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
     this.initMap()
-
-
 
   }
 
@@ -41,31 +42,41 @@ export class MapComponent implements OnInit {
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
-      minZoom: 10,
+      minZoom: 5,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     })
+
+    const onMapClick = e => {
+      L.marker(e.latlng, {icon: this.regIcon}).addTo(this.map).on('mouseover', onMapDblClick)
+     }
+     const onMapDblClick = e => {
+      this.popup
+      .setLatLng(e.latlng)
+      .setContent(e.latlng.toString())
+      .openOn(this.map);
+     }
 
     const jittery = Array(5).fill(this.centroid).map(
       x => [x[0] + (Math.random() - .5)/10, x[1] + (Math.random() - .5)/10 ]
     ).map(
-      x => L.marker(x as L.LatLngExpression, {icon: this.regIcon})
+      x => L.marker(x as L.LatLngExpression, {icon: this.regIcon}).on('mouseover', onMapDblClick)
     ).forEach(
       x => x.addTo(this.map)
     );
 
+    const search =  GeoSearch.GeoSearchControl({
+      provider: new GeoSearch.OpenStreetMapProvider(),
+      position: 'topright',
+      style: 'bar',
+      searchLabel: 'Pesquisar por endereço',
+
+    });
+
+    this.map.addControl(search);
+
     tiles.addTo(this.map)
 
-   const onMapClick = e => {
-      this.popup
-          .setLatLng(e.latlng)
-          .setContent(e.latlng.toString())
-          .openOn(this.map);
-
-          L.marker(e.latlng, {icon: this.regIcon}).addTo(this.map)
-  }
-  this.map.on('click', onMapClick)
-
-
+    this.map.on('click', onMapClick)
 
   }
 

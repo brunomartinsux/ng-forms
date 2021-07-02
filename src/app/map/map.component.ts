2485529ff2d8da91e6e3c4ac7a322add66292ea8
Bruno Luis Martins <zapelim.dev>
2021-07-02 10:01:35 -0300

    polygon

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-import { OpenStreetMapProvider } from 'leaflet-geosearch';
+import { AntPath, antPath } from 'leaflet-ant-path'
 import * as GeoSearch from 'leaflet-geosearch';
 
 @Component({
@@ -14,6 +14,10 @@ export class MapComponent implements OnInit {
   private map: L.Map
   private centroid: L.LatLngExpression = [-26.32505,-48.83116]
   popup = L.popup();
+  polygon = false;
+
+  latlngs = [];
+
 
    regIcon = L.icon({
     iconUrl: 'assets/sign-marker.png',
@@ -23,6 +27,16 @@ export class MapComponent implements OnInit {
     iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
     shadowAnchor: [4, 62],  // the same for the shadow
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+})
+
+polyIcon = L.icon({
+  iconUrl: 'assets/poly-marker.png',
+
+  iconSize:     [18, 22], // size of the icon
+  shadowSize:   [50, 64], // size of the shadow
+  iconAnchor:   [10, 20], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 })
 
   constructor() { }
@@ -47,8 +61,16 @@ export class MapComponent implements OnInit {
     })
 
     const onMapClick = e => {
-      L.marker(e.latlng, {icon: this.regIcon}).addTo(this.map).on('mouseover', onMapDblClick)
-     }
+      if(this.polygon) {
+        this.latlngs.push(e.latlng)
+        L.marker(e.latlng, {icon: this.polyIcon}).addTo(this.map)
+        const options = { use: L.polyline, delay: 400, dashArray: [10,20], weight: 5, color: "#0000FF", pulseColor: "#FFFFFF" };
+        const path = antPath(this.latlngs, options);
+        path.addTo(this.map);
+      } else {
+        L.marker(e.latlng, {icon: this.regIcon, draggable: true}).addTo(this.map).on('click', onMapDblClick)
+      }
+    }
      const onMapDblClick = e => {
       this.popup
       .setLatLng(e.latlng)
@@ -59,9 +81,11 @@ export class MapComponent implements OnInit {
     const jittery = Array(5).fill(this.centroid).map(
       x => [x[0] + (Math.random() - .5)/10, x[1] + (Math.random() - .5)/10 ]
     ).map(
-      x => L.marker(x as L.LatLngExpression, {icon: this.regIcon}).on('mouseover', onMapDblClick)
+      x => L.marker(x as L.LatLngExpression, {icon: this.regIcon, draggable: true}).on('click', onMapDblClick)
     ).forEach(
-      x => x.addTo(this.map)
+      x => {
+        x.addTo(this.map)
+      }
     );
 
     const search =  GeoSearch.GeoSearchControl({
@@ -71,6 +95,7 @@ export class MapComponent implements OnInit {
       searchLabel: 'Pesquisar por endereço',
 
     });
+
 
     this.map.addControl(search);
 
